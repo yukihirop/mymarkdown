972e009746b5942295a9573cf67b739c0ce79741
yukihirop <te108186@gmail.com>
2019-01-21 17:41:55 +0900

    ローディングの時の表示を追加した。

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="top">
+    <div class="loading" v-if="!isLoginChecked">
+      <i class="fa fa-spinner fa-spin fa-lg fa-5x"></i>
+    </div>
     <GlobalNav :user="userData"></GlobalNav>
     <Home v-if="!isLogin"></Home>
     <Editor v-if="isLogin" :user="userData"></Editor>
@@ -27,15 +30,15 @@ export default {
   },
   created: function() {
     firebase.auth().onAuthStateChanged(user => {
-      console.log(user)
+      this.isLoginChecked = true;
       if (user) {
-        this.isLogin = true
-        this.userData = user
+        this.isLogin = true;
+        this.userData = user;
       } else {
-        this.isLogin = false
-        this.userData = null
-      }
-    })
+        this.isLogin = false;
+        this.userData = null;
+      };
+    });
   }
 }
 </script>
@@ -50,23 +53,33 @@ export default {
   color: #2c3e50;
   height: 100%;
 }
-
 h1,
 h2 {
   font-weight: normal;
 }
-
 ul {
   display: inline-block;
   margin: 0 10px;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
+}
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #eee;
+  i {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -0.5em 0 0 -0.5em;
+  }
 }
 </style>
